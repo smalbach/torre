fcd05b506749cdebc2eade4a23d50eec68347cda
Snheidert Smalbach <smalbach24@gmail.com>
2021-08-26 15:39:22 -0500

    Fix no image organization

--- src/components/Career/OrganizationLogo.js
+++ src/components/Career/OrganizationLogo.js
@@ -1,10 +1,28 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
 
 const OrganizationLogo = ({ organizations }) => {
-  const organization = organizations.filter((org) => org.picture !== null);
+  const [picture, setPicture] = useState(null);
+  const [organization, setOrganization] = useState(organizations[0]);
+  //console.log({ organizations });
+  useEffect(() => {
+    try {
+      console.log("Reading org");
+      const auxOrg = organizations.filter((org) => org.picture !== null);
+
+      if (auxOrg) {
+        setOrganization(auxOrg);
+        setPicture(organization.picture);
+      }
+      if (organizations[0].picture) {
+      }
+    } catch (err) {
+      setPicture(null);
+    }
+  }, []);
 
   return (
-    <img src={organization[0].picture} data="wii" alt={organization[0].name} />
+    <>{picture ? <img src={picture} alt={organization[0].name} /> : null}</>
   );
 };
 
